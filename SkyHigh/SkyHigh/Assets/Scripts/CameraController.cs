a91cc2da6b911685dd55155e7f25ff66a76730cb
nishi <icemaunntenn@gmail.com>
2016-03-22 07:29:28 +0900

    プロジェクトの追加

---
+++ SkyHigh/SkyHigh/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    private float angleH;
+    private float angleV;
+    public float rotSpeed = 180f;
+    public float radius = 5f;
+
+    void LateUpdate()
+    {
+        angleH += Input.GetAxis("LeftStickX") * rotSpeed * Time.deltaTime;
+        angleV += Input.GetAxis("LeftStickY") * rotSpeed * Time.deltaTime;
+
+        Vector3 rotDir = Quaternion.Euler(-angleV, -angleH, 0f) * Vector3.back;
+        transform.position = (target.position + new Vector3(0.0f,1.0f,0.0f)) + radius * rotDir;
+
+        transform.LookAt(target.position + new Vector3(0.0f, 1.0f, 0.0f));
+    }
+}
---
+++ SkyHigh/SkyHigh/Assets/Scripts/WindController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindController : MonoBehaviour {
+
+    public float power = 10f;
+    public float coefficient;
+    public GameObject player;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 velocity = Camera.main.transform.forward + new Vector3(0.0f, 2.0f, 0.0f);
+        Vector3 totalv = velocity * (power * Input.GetAxis("Trigger"));
+        totalv.y = Mathf.Abs(totalv.y);
+        Wind(player,totalv);
+    }
+
+    void Wind(GameObject col, Vector3 velocity)
+    {
+        // 相対速度計算
+        var relativeVelocity = velocity - col.GetComponent<Rigidbody>().velocity;
+
+        // 空気抵抗を与える
+        col.GetComponent<Rigidbody>().AddForce(coefficient * relativeVelocity);
+    }
+}
